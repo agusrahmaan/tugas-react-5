b8a25600c10aac5e8aeb44b1321435112ce5e9f6
Wahyu <arh100802@gmail.com>
2023-06-21 22:28:08 +0700

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -34,196 +34,194 @@ function App() {
 
   return (
     <>
-    <header>
-        <TfiVideoClapper size={30}  />
+      <header>
+        <TfiVideoClapper size={30} />
         Tugas 5 & 6
       </header>
-    <div className="main">
-      
-      <div>
-        {kdrama.map((value, index) => {
-          return (
-            <KoreanDrama
-              key={index}
-              id={value.id}
-              title={value.title}
-              episode={value.episode}
-            />
-          );
-        })}
-      </div>
-      <div className="tambah">
-        <form>
-          <h1>Tambah</h1>
-          <div className="input">
-            <label>
-              Id:
-              <input
-                type="number"
-                value={id}
-                onChange={(e) => setId(parseInt(e.target.value))}
+      <div className="main">
+        <div>
+          {kdrama.map((value, index) => {
+            return (
+              <KoreanDrama
+                key={index}
+                id={value.id}
+                title={value.title}
+                episode={value.episode}
               />
-            </label>
-            <label>
-              Title:
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </label>
-            <label>
-              Episode:
-              <input
-                type="number"
-                value={episode}
-                onChange={(e) => setEpisode(e.target.value)}
-              />
-            </label>
-          </div>
-          <div className="btn">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                setKdrama([
-                  { id: id, title: title, episode: episode },
-                  ...kdrama,
-                ]);
-              }}
-            >
-              <FaPlusCircle />
-              Depan
-            </button>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                setKdrama([
-                  ...kdrama,
-                  { id: id, title: title, episode: episode },
-                ]);
-              }}
-            >
-              <FaPlusCircle />
-              Belakang
-            </button>
-          </div>
-        </form>
-      </div>
-      <div className="edit">
-        <form>
-          <h3>Edit berdasarkan ID</h3>
-          <div className="input">
-            <label>
-              Id:
-              <input
-                type="number"
-                value={editId}
-                onChange={(e) => {
+            );
+          })}
+        </div>
+        <div className="tambah">
+          <form>
+            <h1>Tambah</h1>
+            <div className="input">
+              <label>
+                Id:
+                <input
+                  type="number"
+                  value={id}
+                  onChange={(e) => setId(parseInt(e.target.value))}
+                />
+              </label>
+              <label>
+                Title:
+                <input
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </label>
+              <label>
+                Episode:
+                <input
+                  type="number"
+                  value={episode}
+                  onChange={(e) => setEpisode(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="btn">
+              <button
+                onClick={(e) => {
                   e.preventDefault();
-                  setEditId(parseInt(e.target.value));
-                  if (kdrama.find((x) => x.id == e.target.value)) {
-                    kdrama.map((x) => {
-                      if (x.id == e.target.value) {
-                        setEditTitle(x.title);
-                      }
-                    });
-                  } else {
-                    setEditTitle("");
-                  }
+                  setKdrama([
+                    { id: id, title: title, episode: episode },
+                    ...kdrama,
+                  ]);
                 }}
-              />
-            </label>
-            <label>
-              Title:
-              <input
-                type="text"
-                value={editTitle}
-                onChange={(e) => {
+              >
+                <FaPlusCircle />
+                Depan
+              </button>
+              <button
+                onClick={(e) => {
                   e.preventDefault();
-                  setEditTitle(e.target.value);
+                  setKdrama([
+                    ...kdrama,
+                    { id: id, title: title, episode: episode },
+                  ]);
                 }}
-              />
-            </label>
+              >
+                <FaPlusCircle />
+                Belakang
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="edit">
+          <form>
+            <h3>Edit berdasarkan ID</h3>
+            <div className="input">
+              <label>
+                Id:
+                <input
+                  type="number"
+                  value={editId}
+                  onChange={(e) => {
+                    e.preventDefault();
+                    setEditId(parseInt(e.target.value));
+                    if (kdrama.find((x) => x.id == e.target.value)) {
+                      kdrama.map((x) => {
+                        if (x.id == e.target.value) {
+                          setEditTitle(x.title);
+                        }
+                      });
+                    } else {
+                      setEditTitle("");
+                    }
+                  }}
+                />
+              </label>
+              <label>
+                Title:
+                <input
+                  type="text"
+                  value={editTitle}
+                  onChange={(e) => {
+                    e.preventDefault();
+                    setEditTitle(e.target.value);
+                  }}
+                />
+              </label>
 
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                setKdrama(
-                  kdrama.map((x) =>
-                    x.id === editId ? { ...x, title: editTitle } : x
-                  )
-                );
-              }}
-            >
-              {" "}
-              <FiEdit />
-              Edit
-            </button>
-            <br />
-            <label>
-              Episode:
-              <div className="btn-zoom">
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setKdrama(
-                      kdrama.map((x) =>
-                        x.id === editId ? { ...x, episode: x.episode + 1 } : x
-                      )
-                    );
-                  }}
-                >
-                  <MdZoomOutMap />
-                  Perbesar
-                </button>
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setKdrama(
-                      kdrama.map((x) =>
-                        x.id === editId ? { ...x, episode: x.episode - 1 } : x
-                      )
-                    );
-                  }}
-                >
-                  <MdZoomInMap />
-                  Perkecil
-                </button>
-              </div>
-            </label>
-          </div>
-        </form>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  setKdrama(
+                    kdrama.map((x) =>
+                      x.id === editId ? { ...x, title: editTitle } : x
+                    )
+                  );
+                }}
+              >
+                {" "}
+                <FiEdit />
+                Edit
+              </button>
+              <br />
+              <label>
+                Episode:
+                <div className="btn-zoom">
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setKdrama(
+                        kdrama.map((x) =>
+                          x.id === editId ? { ...x, episode: x.episode + 1 } : x
+                        )
+                      );
+                    }}
+                  >
+                    <MdZoomOutMap />
+                    Perbesar
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setKdrama(
+                        kdrama.map((x) =>
+                          x.id === editId ? { ...x, episode: x.episode - 1 } : x
+                        )
+                      );
+                    }}
+                  >
+                    <MdZoomInMap />
+                    Perkecil
+                  </button>
+                </div>
+              </label>
+            </div>
+          </form>
+        </div>
+        <div className="hapus">
+          <h2>Hapus</h2>
+          <button
+            onClick={() => {
+              setKdrama(kdrama.slice(1));
+            }}
+          >
+            <AiFillDelete />
+            Depan
+          </button>
+          <button
+            onClick={() => {
+              setKdrama(kdrama.slice(0, -1));
+            }}
+          >
+            <AiFillDelete />
+            Belakang
+          </button>
+          <button
+            onClick={() => {
+              setKdrama(kdrama.slice(0, 0));
+            }}
+          >
+            <AiFillDelete />
+            Semua
+          </button>
+        </div>
       </div>
-      <div className="hapus">
-        <h2>Hapus</h2>
-        <button
-          onClick={() => {
-            setKdrama(kdrama.slice(1));
-          }}
-        >
-          <AiFillDelete />
-          Depan
-        </button>
-        <button
-          onClick={() => {
-            setKdrama(kdrama.slice(0, -1));
-          }}
-        >
-          <AiFillDelete />
-          Belakang
-        </button>
-        <button
-          onClick={() => {
-            setKdrama(kdrama.slice(0, 0));
-          }}
-        >
-          <AiFillDelete />
-          Semua
-        </button>
-      </div>
-    </div>
     </>
-    
   );
 }
 
